f9dccc4a03783ca019231e6ddd8c313f93ea172a
cbbeard <cbbeard@outlook.com>
2017-05-11 14:49:28 -0500

    initial

---
+++ Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Fix
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Fix))
+        Me.FixButton = New System.Windows.Forms.Button()
+        Me.CloseButton = New System.Windows.Forms.Button()
+        Me.Sig1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'FixButton
+        '
+        Me.FixButton.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.FixButton.Font = New System.Drawing.Font("Impact", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FixButton.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.FixButton.Location = New System.Drawing.Point(70, 51)
+        Me.FixButton.Name = "FixButton"
+        Me.FixButton.Size = New System.Drawing.Size(151, 62)
+        Me.FixButton.TabIndex = 0
+        Me.FixButton.Text = "Fix"
+        Me.FixButton.UseVisualStyleBackColor = False
+        '
+        'CloseButton
+        '
+        Me.CloseButton.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.CloseButton.Font = New System.Drawing.Font("Impact", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CloseButton.Location = New System.Drawing.Point(70, 151)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(151, 62)
+        Me.CloseButton.TabIndex = 1
+        Me.CloseButton.Text = "Close"
+        Me.CloseButton.UseVisualStyleBackColor = False
+        '
+        'Sig1
+        '
+        Me.Sig1.AutoSize = True
+        Me.Sig1.BackColor = System.Drawing.Color.Transparent
+        Me.Sig1.Font = New System.Drawing.Font("Lucida Handwriting", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Sig1.ForeColor = System.Drawing.Color.LightSlateGray
+        Me.Sig1.Location = New System.Drawing.Point(263, 246)
+        Me.Sig1.Name = "Sig1"
+        Me.Sig1.Size = New System.Drawing.Size(23, 15)
+        Me.Sig1.TabIndex = 2
+        Me.Sig1.Text = "CB"
+        '
+        'Fix
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Sig1)
+        Me.Controls.Add(Me.CloseButton)
+        Me.Controls.Add(Me.FixButton)
+        Me.DoubleBuffered = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Location = New System.Drawing.Point(100, 100)
+        Me.Name = "Fix"
+        Me.Text = "Fix Hard Drive"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents FixButton As Button
+    Friend WithEvents CloseButton As Button
+    Friend WithEvents Sig1 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System
+Imports System.IO
+Imports System.Diagnostics
+' RECORD.DAT FIXER BY Chris Beard 3/15/2017
+' DELETES RECORD.DAT AND ENDS ALL CURRENT FLEXPLAY PROCESSES
+' edit 3/27
+
+
+
+Public Class Fix
+    Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles CloseButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub FixButton_Click(sender As Object, e As EventArgs) Handles FixButton.Click
+        ' Get drive info
+        Dim allDrives() As DriveInfo = DriveInfo.GetDrives()
+            Dim d As DriveInfo
+            Dim RECORD As String
+        Dim procsToKill() As String = {"FPL16Pro", "FPL16Player", "FPL18Player", "FPLPro7", "FPLPro7M"}
+        ' Kill current processes to prevent error
+        For Each p As Process In Process.GetProcesses
+            If procsToKill.Contains(p.ProcessName) Then
+                p.Kill()
+            End If
+        Next
+        For Each d In allDrives
+            ' Delete record.dat
+            RECORD = System.IO.Path.Combine(d.Name, "RECORD.DAT")
+            If System.IO.File.Exists(RECORD) = True Then
+                File.SetAttributes(RECORD, FileAttributes.Normal)
+                System.IO.File.Delete(RECORD)
+                MessageBox.Show("Success! Now open Flexplay.")
+            End If
+        Next
+    End Sub
+End Class
